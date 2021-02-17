71e6990672bfddc5fcf51d68babfe6f90dbd3383
Nishat Sanjida <sanjidan1993@gmail.com>
2021-02-17 23:36:33 +0600

    1st

--- script.js
+++ script.js
@@ -6,41 +6,26 @@ loginForm.addEventListener('submit', function (e) {
   let username = document.forms['loginForm'].username.value;
   let pass = document.forms['loginForm'].pass.value;
 
-  if (username.trim() == '') {
+  if (username.trim() == '' || pass.trim() == '') {
     e.preventDefault();
-    userField.classList.add('validate-text');
-    userField.innerHTML = 'Username field must not be empty';
-    passField.classList.remove('validate-text');
-    passField.innerHTML = '';
-  } else if (pass.trim() == '') {
-    e.preventDefault();
-    passField.classList.add('validate-text');
-    passField.innerHTML = 'Password field must not be empty';
-    userField.classList.remove('validate-text');
-    userField.innerHTML = '';
-  }
-  // if (username.trim() == '' || pass.trim() == '') {
-  //   e.preventDefault();
-  //   userField.classList.add('validate-text');
-  //   userField.innerHTML = 'Username field must not be empty';
-  //   passField.classList.add('validate-text');
-  //   passField.innerHTML = 'Password field must not be empty';
 
-  //   if (username.trim() == '') {
-  //     userField.classList.add('validate-text');
-  //     userField.innerHTML = 'Username field must not be empty';
-  //     passField.classList.remove('validate-text');
-  //     passField.innerHTML = '';
-  //   }
+    if (username.trim() == '') {
+      userField.classList.add('validate-text');
+      userField.innerHTML = 'Username field must not be empty';
+    } else {
+      userField.classList.remove('validate-text');
+      userField.innerHTML = '';
+    }
 
-  //   if (pass.trim() == '') {
-  //     passField.classList.add('validate-text');
-  //     passField.innerHTML = 'Password field must not be empty';
-  //     userField.classList.remove('validate-text');
-  //     userField.innerHTML = '';
-  //   }
-  // }
-  else {
+    if (pass.trim() == '') {
+      passField.classList.add('validate-text');
+      passField.innerHTML = 'Password field must not be empty';
+    } 
+    else {
+      passField.classList.remove('validate-text');
+      passField.innerHTML = '';
+    }
+  } else {
     userField.classList.remove('validate-text');
     passField.classList.remove('validate-text');
     userField.innerHTML = '';
